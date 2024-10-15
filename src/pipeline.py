5e2a7c931b836eed1dc797a3f6fc20f10922c964
Robert Meppelink <robert@meppelink.net>
2024-10-15 16:15:18 +0000

    4

--- src/pipeline.py
+++ src/pipeline.py
@@ -1321,6 +1321,6 @@ def infer(request: TextToImageRequest, pipeline: StableDiffusionXLPipeline) -> I
         width=request.width,
         height=request.height,
         generator=generator,
-        num_inference_steps=6,
+        num_inference_steps=4,
     ).images[0]
 
